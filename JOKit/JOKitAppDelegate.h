97ee9cfc90810014b38d5540cf3b150227c7f578
Jeffrey Oloresisimo <jeffrey.oloresisimo@gmail.com>
2011-08-23 22:50:03 -0700

    Initial Commit

---
+++ JOKit/JOKitAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  JOKitAppDelegate.h
+//  JOKit
+//
+//  Created by Jeffrey Oloresisimo on 11-08-23.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOKitAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
